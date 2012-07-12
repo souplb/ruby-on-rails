ea4e3878c1cfdc0f07d35ab165af5f806443e472
souplb <lambertbin.liu@gmail.com>
2012-07-13 01:06:58 +1000

    first ruby-on-rails running with unicorn on nginx

---
+++ testApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'c29da4a6f4082b6c7ec81d784ee6b0b7109dd2d6c9e2e520dc4e2c3ad34682de07ddb985d98eee7084afae45b6849ab86e2db879f04cf6b69be6da20db1de63e'
